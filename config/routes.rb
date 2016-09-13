ccba902b949f1fcab3fce9c12f7328d3a7534c84
nsellek <nick@seansellek.com>
2016-09-13 17:26:47 -0400

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destory'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "signup" => "users#new"
+  post 'users' => "users#create"
+
+  get "login" => "session#new"
+  post "login" => "session#create"
+  get "logout" => "session#destory"
 end
