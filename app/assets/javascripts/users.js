994f36ba919af9a19ace658e485df82d4b635f5c
nsellek <nick@seansellek.com>
2016-09-21 09:45:58 -0400

    added server side email validation and added password validation code for future use

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,28 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// function checkPasswordMatch() {
+//   var pass1 = $("#pass1");
+//   var pass2 = $("#pass2");
+//   var message = $("#confirmMessage");
+//   var goodColor = "#66cc66";
+//   var badColor = "#ff6666";
+//   var btn = $("#submit");
+//
+//   if(pass1.val() == pass2.val()){
+//     pass2.style.backgroundColor = goodColor;
+//     message.style.color = goodColor;
+//     message.innerHTML = "Passwords Match!";
+//     btn.prop('disabled', false);
+//   } else{
+//       pass2.style.backgroundColor = badColor;
+//       message.style.color = badColor;
+//       message.innerHTML = "Passwords Do Not Match!";
+//       btn.prop('disabled', true);
+//   }
+// }
+//
+// $(document).on("turbolinks:load", function(){
+//   $("#pass1, #pass2").keyup(checkPasswordMatch);
+// });
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :user_name, :email, presence: true
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :email, uniqueness: true
   has_secure_password
   has_one :guild
