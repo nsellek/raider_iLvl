7be4566afcb40fc0044d772a9a4153a9b89bd1b8
nsellek <nick@seansellek.com>
2016-10-10 13:49:11 -0400

    added javascript to disable/enable members button depending on if any boxes are checked

--- app/assets/javascripts/raid_groups.js
+++ app/assets/javascripts/raid_groups.js
@@ -1,7 +1,27 @@
-$(document).on('turbolinks:load', function() {
+main = function(){
+  listSearch();
+  buttonCheck();
+}
+
+listSearch = function(){
   var options = {
     valueNames: [ 'name' ]
   };
 
   var userList = new List('members', options);
-});
+}
+
+buttonCheck = function(){
+  var chkbox = $(".add_member");
+  var btn = $(".add_members");
+  btn.attr("disabled", "disabled");
+  chkbox.change(function(){
+    if(this.checked){
+      btn.removeAttr("disabled");
+    } else{
+      btn.attr("disabled", "disabled");
+    }
+  });
+};
+
+$(document).on('turbolinks:load', main());
