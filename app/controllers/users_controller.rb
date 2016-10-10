6265a4ce81276fdb89d18c0896e28734e9dfb062
nsellek <nick@seansellek.com>
2016-10-10 14:53:30 -0400

    Errors when signing up now show on signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(allowed_user_params)
 
-    if @user.save!
+    if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      redirect_to signup_path
+      render :new
     end
   end
 
