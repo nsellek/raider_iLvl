86c59e5d19a2cc05589ba56e3041519985228a2f
nsellek <nick@seansellek.com>
2016-10-13 14:55:12 -0400

    displays error message when user fails to use correct email or password

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,14 +1,16 @@
 class SessionController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path
-    else
       redirect_to dashboard_path
+    else
+      flash.now[:error] = "Invalid email or password"
+      render :new
     end
   end
 
