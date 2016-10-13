d068573124031f4684efd2511a45b0df997c2a27
nsellek <nick@seansellek.com>
2016-10-13 13:02:13 -0400

    Changed the login redirect action to take you to the dashboard insead of home page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,7 +8,7 @@ class SessionController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      redirect_to login_path
+      redirect_to dashboard_path
     end
   end
 
