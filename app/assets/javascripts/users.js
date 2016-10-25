0edbe7a9dbe99779cfbc2bb4554823efcbe467ee
nsellek <nick@seansellek.com>
2016-10-25 14:00:28 -0400

    added password validation to signup page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,27 +2,30 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-// function checkPasswordMatch() {
-//   var pass1 = $("#pass1");
-//   var pass2 = $("#pass2");
-//   var message = $("#confirmMessage");
-//   var goodColor = "#66cc66";
-//   var badColor = "#ff6666";
-//   var btn = $("#submit");
-//
-//   if(pass1.val() == pass2.val()){
-//     pass2.style.backgroundColor = goodColor;
-//     message.style.color = goodColor;
-//     message.innerHTML = "Passwords Match!";
-//     btn.prop('disabled', false);
-//   } else{
-//       pass2.style.backgroundColor = badColor;
-//       message.style.color = badColor;
-//       message.innerHTML = "Passwords Do Not Match!";
-//       btn.prop('disabled', true);
-//   }
-// }
-//
-// $(document).on("turbolinks:load", function(){
-//   $("#pass1, #pass2").keyup(checkPasswordMatch);
-// });
+main = function(){
+  checkPasswordMatch();
+}
+
+checkPasswordMatch = function() {
+  var pass1 = $("#pass1");
+  var pass2 = $("#pass2");
+  var btn = $("#submit");
+  btn.attr("disabled", "disabled");
+
+  pass1.keyup(function(){
+    if(pass1.val() == pass2.val()){
+      btn.removeAttr("disabled");
+    } else{
+      btn.attr("disabled", "disabled");
+    }
+  });
+  pass2.keyup(function(){
+    if(pass1.val() == pass2.val()){
+      btn.removeAttr("disabled");
+    } else{
+      btn.attr("disabled", "disabled");
+    }
+  });
+}
+
+$(document).on("turbolinks:load", main());
