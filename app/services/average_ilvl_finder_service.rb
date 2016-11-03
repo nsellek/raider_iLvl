846c165f8d04390c5d9f34e450d35955a3680627
nsellek <nick@seansellek.com>
2016-11-03 12:15:48 -0400

    removed useless variable

--- app/services/average_ilvl_finder_service.rb
+++ app/services/average_ilvl_finder_service.rb
@@ -1,10 +1,9 @@
 class AverageIlvlFinderService
   def self.find_average(members)
-    average = 0
     members_ilvl = []
     members.each do |member|
       members_ilvl << member.items["averageItemLevelEquipped"].to_i
     end
-    average = members_ilvl.inject(:+)/members.length
+    members_ilvl.inject(:+)/members.length
   end
 end
