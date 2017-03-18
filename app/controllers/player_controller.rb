0139f25a86afa59de3d155d741f0ba750ca680e8
nsellek <nick@seansellek.com>
2017-03-17 20:04:20 -0400

    removed unneeded files, added a refresh button to each raid group so theres no need to open the group, set up wowhead.com tooltips

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,7 +2,7 @@ class PlayerController < ApplicationController
   before_filter :find_player, except: []
   def view_player
     session[:return_to] ||= request.referer
-    @stat_list = {3 => "Agility", 4=> "Strength", 5=> "Intellect", 6=> "Spirit", 7=> "Stamina", 32=> "Critical Strike", 36=> "Haste", 40=> "Versatility", 49=> "Mastery", 62=> "Leech", 63=> "Avoidance", 71=> "Agility or Strength or Intellect", 73=> "Agility or Intellect", 74=> "Strength or Intellect"}
+    # @stat_list = {3 => "Agility", 4=> "Strength", 5=> "Intellect", 6=> "Spirit", 7=> "Stamina", 32=> "Critical Strike", 36=> "Haste", 40=> "Versatility", 49=> "Mastery",61=> "Speed", 62=> "Leech", 63=> "Avoidance", 71=> "Agility or Strength or Intellect",72=> "Strength or Agility", 73=> "Agility or Intellect", 74=> "Strength or Intellect"}
     @items = @player.items.drop(2)
   end
 
